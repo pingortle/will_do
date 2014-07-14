88428a717c4bd484327fca109f6802cee9e19bba
Kaleb Lape <pingortle@gmail.com>
2014-07-14 10:07:08 -0500

    Robustify creating events.

--- client/app.js
+++ client/app.js
@@ -1,10 +1,5 @@
 Meteor.subscribe('events');
 
-var resetNewEvent = function() {
-	Session.set("newEventTime", "12:00");
-	Session.set("newEventTitle", "");
-}
-
 var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 Template.calendar.helpers({
@@ -53,41 +48,32 @@ Template.day.events({
 });
 
 Template.add_event_modal.helpers({
-	eventTime: function() {
-		return Session.get("newEventTime");
-	},
 	eventDate: function() {
 		return moment(Session.get("selectedDay")).format("dddd, MMMM Do YYYY");
-	},
-	title: function() {
-		return Session.get("newEventTitle");
 	},
 });
 
 Template.add_event_modal.events({
 	'click button.new-event': function() {
+		var titleField = $('#new_event_title')[0];
+		var timeField = $('#new_event_time')[0];
+
 		var date = moment(Session.get('selectedDay'));
-		var time = moment(Session.get('newEventTime'), "HH:mm");
+		var time = moment(timeField.value, "HH:mm");
 		date.startOf('day').hour(time.hour());
 		date.minute(time.minute());
 		var obj = {
-			name: Session.get('newEventTitle'),
+			name: titleField.value,
 			date: date.toDate(),
 		};
 		Events.insert(obj);
 
-		resetNewEvent();
+		titleField.value = '';
+		timeField.value = '';
 		$('#add_event_modal').modal('hide');
-	},
-	'blur input[type="text"]': function(e) {
-		if (e.target.id === 'new_event_title')
-			Session.set('newEventTitle', e.target.value);
-		else
-			Session.set('newEventTime', e.target.value);
 	},
 });
 
 Template.add_event_modal.rendered = function () {
-    $('.clockpicker').clockpicker();
-    resetNewEvent();
+		$('.clockpicker').clockpicker();
 };
